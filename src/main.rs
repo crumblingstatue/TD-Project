609925d6fed25b2db84531d2ac6152c0cab893bd
crumblingstatue <radiantstatue@gmail.com>
2021-05-22 21:57:57 +0200

    Add enemy pathing

--- src/main.rs
+++ src/main.rs
@@ -25,10 +25,47 @@ impl Path {
     }
 }
 
+struct Enemy {
+    pos: Vec2,
+    path_pos: usize,
+}
+
+impl Enemy {
+    pub fn debug_draw(&self) {
+        draw_circle(self.pos.x, self.pos.y, 4.0, GREEN);
+    }
+    pub fn spawn_on_path(path: &Path) -> Self {
+        Self {
+            pos: path.nodes[0],
+            path_pos: 0,
+        }
+    }
+    pub fn advance(&mut self, path: &Path) {
+        let next = match path.nodes.get(self.path_pos) {
+            Some(next) => *next,
+            None => return,
+        };
+        let speed = 2.4;
+        let diff = next - self.pos;
+        let distance_sq = diff.x.powf(2.0) + diff.y.powf(2.0);
+        if distance_sq.sqrt() < speed {
+            self.path_pos += 1;
+            return;
+        }
+        let angle = diff.y.atan2(diff.x);
+        let x_move = angle.cos() * speed;
+        let y_move = angle.sin() * speed;
+        self.pos.x += x_move;
+        self.pos.y += y_move;
+        draw_line(self.pos.x, self.pos.y, next.x, next.y, 2.0, BLUE);
+    }
+}
+
 #[macroquad::main("TD Project")]
 async fn main() {
     let mut path = Path::default();
     let mut editing = true;
+    let mut enemies = Vec::new();
     loop {
         let (mx, my) = mouse_position();
         if is_mouse_button_pressed(MouseButton::Left) && editing {
@@ -40,7 +77,14 @@ async fn main() {
         if is_key_pressed(KeyCode::E) {
             editing = !editing;
         }
+        if is_key_pressed(KeyCode::S) && !path.nodes.is_empty() {
+            enemies.push(Enemy::spawn_on_path(&path));
+        }
         path.debug_draw();
+        for enemy in &mut enemies {
+            enemy.advance(&path);
+            enemy.debug_draw();
+        }
         if editing {
             path.project_next_at_mouse();
         }
